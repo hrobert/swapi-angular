69c60ce6fa32678916ceaa436417bac21ea6af58
Hitinui Robert <hitinui.robert@gmail.com>
2021-07-11 15:04:05 -1000

    Add basic back navigation

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private location: Location) { }
 
   ngOnInit(): void {
   }
 
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
+
+  back(): void {
+    this.location.back();
+  }
 }
